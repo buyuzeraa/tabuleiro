f79ee231a852080a4a76ae5a89f9519aab82ae1b
Luan Henrique Dos Anjos Barbosa Brito <buyuzeraa@gmail.com>
2026-05-12 15:42:33 +0000

    int

---
+++ xadrez.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main (){
+//rainha deve mover oito casas pra esquerda
+//bispo deve mover cinco casas na diagonal pra cima e direita
+//torre deve mover 5 casas pra direita
+
+
+    printf("Escolha a peça que você quer mover");
+    printf("rainha");
+    printf("bispo");
+    printf("torre");
+    scanf("%s", peca);
+
+switch (peca)
+
+    case 'rainha'{
+        for (i=0, i<=5, i++)
+        printf(esquerda);
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
